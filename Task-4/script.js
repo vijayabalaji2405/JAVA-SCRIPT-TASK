9ccc2b213e3c06711e1198c15a5419ab5a10fc62
vijayabalaji <vijaya.balaji@codem.com>
2021-12-27 14:46:18 +0530

    Completed

---
+++ Task-4/script.js
@@ -0,0 +1,7 @@
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill value was ${bill} ,the tip was ${tip} , and the total value ${
+    bill + tip
+  }`
+);
