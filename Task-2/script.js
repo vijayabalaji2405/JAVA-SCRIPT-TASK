4dd77e32877b945d0cd8869e1e3185a7d92adc6f
vijayabalaji <vijaya.balaji@codem.com>
2022-01-03 11:54:33 +0530

    completed

--- Task-2/script.js
+++ Task-2/script.js
@@ -57,10 +57,9 @@ for (const [team, odd] of Object.entries(game.odds)) {
   console.log(`Odd of ${teamStr} ${odd}`);
 }
 
-// ................doubt.................
-
-// const scorers = {};
-// for (const player of game.scored) {
-//   scorers[player] ? scorers[player]++ : (scorers[player] = 1);
-// }
-// console.log(scorers);
+//4
+const scorers = {};
+for (const player of game.scored) {
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1);
+}
+console.log(scorers);
